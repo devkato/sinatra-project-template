9c4e853a4089e60a4c2141cffda63d3aea88f2a0
Hiroyuki Kato <kato@qpon.jp>
2011-02-27 03:07:47 +0900

    hoge

--- app.rb
+++ app.rb
@@ -1,21 +1,36 @@
 # -*- coding: utf-8 -*-
 
+# ======================================================================
+#
+# This class is 'modular' type template.
+#
+# ======================================================================
 class AppTemplate < Sinatra::Base
   set :sessions, false
   set :logging, true
   set :dump_errors, true
-  #set :some_custom_option, false
+  set :some_custom_option, false
   set :public, './public'
 
+
+  # reload this class on development environment.
   configure(:development) do
     register Sinatra::Reloader
   end
 
+
+  # ----------------------------------------------------------------------
+  # direct response template
+  # ----------------------------------------------------------------------
   get '/' do
     puts 'show index'
     'index'
   end
 
+
+  # ----------------------------------------------------------------------
+  # erb template
+  # ----------------------------------------------------------------------
   get '/index' do
     erb :index
   end
